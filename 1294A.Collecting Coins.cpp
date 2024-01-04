81e9ae24ba083b2cb7542b17b7081f14b4ff4b77
Md.Raju Mia <70192276+Md-Raju-Mia@users.noreply.github.com>
2024-01-04 17:17:49 +0600

    Problem Solution

---
+++ 1294A.Collecting Coins.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,c,n;
+        cin>>a>>b>>c>>n;
+
+        int total=a+b+c+n;
+
+        if(total%3!=0)
+            cout<<"NO"<<endl;
+        else
+        {
+            if(a>total/3 || b>total/3 || c>total/3)
+                cout<<"NO"<<endl;
+            else
+                cout<<"YES"<<endl;
+        }
+    }
+
+    return 0;
+}
