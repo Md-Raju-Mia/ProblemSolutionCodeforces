335583f4c45e9b4f3c9c57a012f92ac3c6f50b0b
Md.Raju Mia <70192276+Md-Raju-Mia@users.noreply.github.com>
2024-01-05 11:00:51 +0600

    Problem Solution

---
+++ 959A.Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int n;
+    cin>>n;
+    if(n%2==1)
+        cout<<"Ehab"<<endl;
+    else
+    {
+        cout<<"Mahmoud"<<endl;
+    }
+
+    return 0;
+}
