204ffd526e8e0ee27a498bde91250b2bc1dc78c0
Md.Raju Mia <70192276+Md-Raju-Mia@users.noreply.github.com>
2024-01-06 12:23:52 +0600

    Problem Solution

---
+++ 1542A.Odd Set.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[2*n];
+        int even=0,odd=0;
+        for(int i=0;i<2*n;i++)
+        {
+            cin>>a[i];
+            if(a[i] % 2 ==0)
+                even++;
+            else
+                odd++;
+        }
+
+        if(even==odd)
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+
+    }
+    return 0;
+}
